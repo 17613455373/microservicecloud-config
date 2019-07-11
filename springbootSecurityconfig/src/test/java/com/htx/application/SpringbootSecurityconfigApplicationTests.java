73ec0715df0106c5b02762673c742db70ac49803
unknown <you@example.com>
2019-07-11 19:45:37 +0800

    Springboot Security

---
+++ springbootSecurityconfig/src/test/java/com/htx/application/SpringbootSecurityconfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.htx.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSecurityconfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
